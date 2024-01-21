68be6c99de3df6ac7aae041b1886caa2099b778f
Kyle <kyle201817146@gmail.com>
2024-01-22 00:27:14 +0800

    DebugServer + graph/description complete

--- Sources/DebugClient/ContentView.swift
+++ Sources/DebugClient/ContentView.swift
@@ -6,23 +6,26 @@
 //
 
 import AGDebugKit
+import os.log
 import Socket
 import SwiftUI
 
 @available(macOS 14.0, *)
 struct ContentView: View {
+    private let logger = Logger(subsystem: "org.OpenSwiftUIProject.AGDebugKit", category: "DebugClient")
+    
     @State private var started = false
     @State private var timeout = 1
 
-    @State private var address = ""
+    @State private var host = ""
     @State private var port: UInt16 = 0
     
     @State private var socket: Socket?
     private var connectServerDisable: Bool {
-        IPv4Address(rawValue: address) == nil || port == 0 || !started
+        IPv4Address(rawValue: host) == nil || port == 0 || !started
     }
     
-    @State private var token: Int = 0
+    @State private var token = 0
     private var serverIODisable: Bool {
         socket == nil || token == 0
     }
@@ -34,6 +37,23 @@ struct ContentView: View {
                 Button {
                     DebugServer.shared.start()
                     started = DebugServer.shared.startSuccess
+                    if started,
+                       let url = DebugServer.shared.url,
+                       let components = URLComponents(url: url, resolvingAgainstBaseURL: false) {
+                        if let host = components.host {
+                            self.host = host
+                        }
+                        if let port = components.port {
+                            self.port = UInt16(port)
+                        }
+                        if let queryItems = components.queryItems,
+                           let tokenItem = queryItems.first(where: { $0.name == "token" }),
+                           let tokenValue = tokenItem.value,
+                           let token = Int(tokenValue) {
+                            self.token = token
+                        }
+                    }
+                    
                 } label: {
                     Text("Start debug server")
                 }
@@ -43,7 +63,7 @@ struct ContentView: View {
                 Stepper("Timeout \(timeout)", value: $timeout)
             }
             Section {
-                TextField("Address", text: $address)
+                TextField("Host", text: $host)
                 TextField("Port", value: $port, formatter: NumberFormatter())
                 Button("Connect server") {
                     Task { try await connectServer() }
@@ -68,58 +88,79 @@ struct ContentView: View {
     }
     
     func connectServer() async throws {
-        guard let addr = IPv4Address(rawValue: address) else {
+        guard let addr = IPv4Address(rawValue: host) else {
             return
         }
         let socket = try await Socket(IPv4Protocol.tcp)
+        self.socket = socket
         do {
             try await socket.connect(to: IPv4SocketAddress(address: addr, port: port))
         } catch {
-            print(error.localizedDescription)
+            logger.error("\(error.localizedDescription, privacy: .public)")
             throw error
         }
-        self.socket = socket
     }
     
-    func writeData() async throws {
-        guard let socket else { return }
+    struct DebugServerMessageHeader: Codable {
+        var token: UInt32
+        var unknown: UInt32
+        var length: UInt32
+        var unknown2: UInt32
+        init(token: UInt32, length: UInt32) {
+            self.token = token
+            self.unknown = 0
+            self.length = length
+            self.unknown2 = 0
+        }
         
-        let command = ["command": "graph/description"]
+        static var size: Int { MemoryLayout<Self>.size }
+    }
+    
+    /// "graph/description" command is the same as `Graph().dict`
+    func writeData(command: String = "graph/description") async throws {
+        guard let socket else { return }
+        let command = ["command": command]
         let commandData = try JSONSerialization.data(withJSONObject: command)
         let size = commandData.count
         
-        struct Header: Codable {
-            var token: UInt32
-            var unknown: UInt32
-            var length: UInt32
-            var unknown2: UInt32
-            init(token: UInt32, length: UInt32) {
-                self.token = token
-                self.unknown = 0
-                self.length = length
-                self.unknown2 = 0
-            }
+        let header = DebugServerMessageHeader(token: UInt32(token), length: UInt32(size))
+        let headerData = withUnsafePointer(to: header) {
+            Data(bytes: UnsafeRawPointer($0), count: DebugServerMessageHeader.size)
         }
-        let header = Header(token: UInt32(token), length: UInt32(size))
-        let headerData = try JSONEncoder().encode(header)
         do {
             let byteCount = try await socket.write(headerData)
-            print("Send: \(byteCount) bytes")
+            logger.info("Send: \(byteCount, privacy: .public) bytes")
         } catch {
-            print(error.localizedDescription)
+            logger.error("\(error.localizedDescription, privacy: .public)")
+            throw error
         }
         do {
             let byteCount = try await socket.write(commandData)
-            print("Send: \(byteCount) bytes")
+            logger.info("Send: \(byteCount, privacy: .public) bytes")
         } catch {
-            print(error.localizedDescription)
+            logger.error("\(error.localizedDescription, privacy: .public)")
+            throw error
         }
     }
     
     func readData() async throws {
         guard let socket else { return }
-        let data = try await socket.read(16)
-        let string = String(data: data, encoding: .utf8) ?? ""
-        print("Received: \(string)")
+        let headerData = try await socket.read(DebugServerMessageHeader.size)
+        
+        let header = headerData.withUnsafeBytes { pointer in
+            pointer.baseAddress!
+                .assumingMemoryBound(to: DebugServerMessageHeader.self)
+                .pointee
+        }
+        guard header.token == token else {
+            logger.error("Token mismatch: header's token-\(header.token, privacy: .public) token-\(token)")
+            return
+        }
+        let size = header.length
+        let dictionaryData = try await socket.read(Int(size))
+        let dict = try JSONSerialization.jsonObject(with: dictionaryData) as? NSDictionary
+        if let dict {
+            print(dict)
+        }
     }
 }
